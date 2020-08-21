e2c50599f3bfd3c32a2990d60b3b7c5a82705d17
Xavier Ruiz <69017482+FHN-Kodiris@users.noreply.github.com>
2020-08-20 23:38:26 -0500

    Base React Commit

---
+++ src/MyInfo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Xavier Ruiz</h1>
+            <ul>
+                <li>
+                    Saxophone
+                </li>
+                <li>
+                    Web Development
+                </li>
+                <li>
+                    Gaming
+                </li>
+            </ul>
+        </div>
+    )
+}
+export default MyInfo
